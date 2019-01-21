73f483ab77c8b71bcef71f8c26ab5aa6a4662881
jmcdevittm <johnmcdevittmallard@users.noreply.github.com>
2019-01-20 20:20:39 -0500

    first commit

---
+++ git.m
@@ -0,0 +1,15 @@
+function [argOut] = git(argIn)
+%UNTITLED2 Executes a git command without having to use system or !
+% git <args>, Command: executes the git command and returns git outputs to
+% MATLAB console.
+%
+% status = git(args), Function: executes the git command and returns git
+% output status.
+%
+% [status, output] = git(args), Function: executes the git command; returns
+% git output status and git output.
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
